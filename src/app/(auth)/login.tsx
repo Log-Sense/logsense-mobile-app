9eca1e938960200aa012541cb21eea125a148b92
Ismael <ismael.henrique.dev@gmail.com>
2025-02-09 20:28:51 -0300

    feat(login): update input placeholder

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,17 +1,20 @@
 import { Text, View } from 'react-native'
 import { Button } from '@/components/button'
 import { Input } from '@/components/input'
+import { router } from 'expo-router'
 
 export default function Login() {
   return (
     <View className='flex flex-1 gap-6 px-6 items-center justify-center'>
       <View className='flex w-full gap-6'>
         <Text className='text-left font-semibold text-xl'>Login</Text>
-        <Text className='text-sm font-semibold text-zinc-600'>Email e senha necessários para a autenticação</Text>
+        <Text className='text-sm font-semibold text-zinc-600'>
+          Email e senha necessários para a autenticação
+        </Text>
       </View>
       <View className='flex w-full items-center flex-col gap-4'>
-        <Input placeholder='Email' />
-        <Input placeholder='Senha' />
+        <Input placeholder='Informe seu email' />
+        <Input placeholder='Informe sua senha' />
       </View>
       <Text className='text-sm font-semibold underline text-zinc-600'>
         Esqueci a senha
@@ -19,7 +22,12 @@ export default function Login() {
       <Button>Confirmar</Button>
       <Text className='text-sm font-semibold text-zinc-600'>
         Ainda não tenho uma conta -{' '}
-        <Text className='underline'>Cadrastar-se</Text>
+        <Text
+          className='underline'
+          onPress={() => router.navigate('/register')}
+        >
+          Cadrastar-se
+        </Text>
       </Text>
     </View>
   )
